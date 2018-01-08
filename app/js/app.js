359ebb369701fb6a667e2c2aefac244a86d35c6f
Igor Kurkov <kurkov87@gmail.com>
2018-01-08 17:01:07 +0200

    add functionality fo listen parameters from url

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,15 @@
 import { selectors } from './inc/_selectors.js'
 import * as render  from './render.js'
 import * as request from './request.js'
-import * as listen from './listen.js'
+import * as listen  from './listen.js'
 
 
 listen.isInputPopulate(request, render); //init
+
+
+
+
+
 
 
 //make recent block
--- app/js/listen.js
+++ app/js/listen.js
@@ -1,14 +1,26 @@
 import { selectors } from './inc/_selectors.js'
 
 export const isInputPopulate = (request, render) => {
-  selectors.input.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-      if(selectors.input.value.length == 0) { 
-        console.log("you dont type city"); return;
-      } else {
-        request.searchWeather(selectors.input.value, "celsius", render.renderWeather)
+  let city = isCityInUrl();
+  if (city) {
+    request.searchWeather(city, "celsius", render.renderWeather);
+    selectors.input.value = city;
+  } 
+  else {
+    selectors.input.addEventListener('keyup', (event) => {
+      if (event.keyCode === 13) {
+        if(selectors.input.value.length == 0) { 
+          console.log("you dont type city"); return;
+        } else {
+          request.searchWeather(selectors.input.value, "celsius", render.renderWeather)
 
-        }
-    }
-  });
+          }
+      }
+    });
+  }
+}
+
+const isCityInUrl = () => {
+  let cityFromUrl = decodeURI(location.search.substring(2))
+  return (cityFromUrl.length > 0) ? cityFromUrl : null;
 }
