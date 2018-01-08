641fd323e40c08b480349c0a4af4b80a0df6e900
Igor Kurkov <kurkov87@gmail.com>
2018-01-08 02:57:44 +0200

    add less instead sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp           = require('gulp'),
-    browserSync    = require('browser-sync'),
-    notify         = require('gulp-notify'),
-    sass           = require('gulp-sass');
-
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    notify      = require('gulp-notify'),
+    sass        = require('gulp-sass'),
+    less        = require('gulp-less'),
+    path        = require('path');
 
 gulp.task("start-server", () => {
   browserSync({
@@ -13,16 +14,27 @@ gulp.task("start-server", () => {
 gulp.task("reload", () => {
   browserSync.reload({stream: true});
 })
-
-gulp.task('sass', () => {
-  return gulp.src('app/sass/**/*+(.sass|.scss)')
-      .pipe(sass().on("error", notify.onError()))
-      .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({stream: true}));
+ 
+gulp.task('less', () => {
+  return gulp.src('app/less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task("default", ["sass", "start-server"], () => {
-  gulp.watch('app/sass/**/*.sass', ["sass", "reload"]);
+
+gulp.task("default", ["less", "start-server"], () => {
+  gulp.watch('app/css/**/*.css', ["reload"]);
+  gulp.watch('app/less/**/*.less', ["less", "reload"]);
   gulp.watch('app/js/**/*.js', ["reload"]);
   gulp.watch('app/**/*.html', ["reload"]);
 })
+
+// gulp.task('sass', () => {
+//   return gulp.src('app/sass/**/*+(.sass|.scss)')
+//       .pipe(sass().on("error", notify.onError()))
+//       .pipe(gulp.dest('app/css'))
+//       .pipe(browserSync.reload({stream: true}));
+// });
