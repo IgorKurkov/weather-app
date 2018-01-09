c2eb82b05d6b53692542d29176941d641706c181
Igor Kurkov <kurkov87@gmail.com>
2018-01-09 04:49:24 +0200

    add button back next listener

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,16 @@ import { selectors } from './inc/_selectors.js'
 import * as render  from './render.js'
 import * as request from './request.js'
 import * as listen  from './listen.js'
-import * as storage  from './storage.js'
-
-listen.isInputPopulate(request, render, storage); //init
 
 
 
-    
 
-//make recent block
-//push current city to recent block
+listen.isInputPopulate(request, render); //init
+
+window.onpopstate = function() {
+  listen.isInputPopulate(request, render); //init
+}
+
+
+
+
