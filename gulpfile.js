9a5ec50151708157932bf18f906792efb6d699d2
Igor Kurkov <kurkov87@gmail.com>
2018-01-11 02:26:39 +0200

    build dist for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,8 @@ gulp.task("default", ["less", "start-server"], () => {
   gulp.watch('app/**/*.html', ["reload"]);
 })
 
+
+
 // gulp.task('sass', () => {
 //   return gulp.src('app/sass/**/*+(.sass|.scss)')
 //       .pipe(sass().on("error", notify.onError()))
@@ -46,32 +48,65 @@ gulp.task("default", ["less", "start-server"], () => {
 
 
 
-var sourceDir = './src';
-var buildDir = './build';
+// var sourceDir = './src';
+// var buildDir = './build';
  
-var Del = require('del');
-var ESLlint = require('gulp-eslint');
-var Sourcemaps = require('gulp-sourcemaps');
-var Export = require('gulp-export');
-var Babel = require('gulp-babel');
+// var Del = require('del');
+// var ESLlint = require('gulp-eslint');
+// var sourcemaps = require('gulp-sourcemaps');
+// var Export = require('gulp-export');
+// var Babel = require('gulp-babel');
  
-gulp.task('clean', cb => {
-  return Del([buildDir], cb);
-});
+// gulp.task('clean', cb => {
+//   return Del([buildDir], cb);
+// });
  
-gulp.task('js-compile', ['clean'], function() {
-  return gulp.src([`${sourceDir}/**/*.js`])
-    // .pipe(ESLlint())
-    // .pipe(ESLlint.format())
-    // .pipe(ESLlint.failAfterError())
-    .pipe(Export({
-        context: './src',
-        exclude: /_/,           // excluded all files with underscore
-        exportType: 'default',  // export as default can be: named, default and global
-    }))
-    .pipe(Sourcemaps.init())
-    .pipe(Babel())
-    .pipe(Sourcemaps.write('.'))
-    .pipe(gulp.dest(buildDir));
-});
+// gulp.task('js-compile', ['clean'], function() {
+//   return gulp.src([`${sourceDir}/**/*.js`])
+//     // .pipe(ESLlint())
+//     // .pipe(ESLlint.format())
+//     // .pipe(ESLlint.failAfterError())
+//     .pipe(Export({
+//         context: './src',
+//         exclude: /_/,           // excluded all files with underscore
+//         exportType: 'default',  // export as default can be: named, default and global
+//     }))
+//     .pipe(sourcemaps.init())
+//     .pipe(Babel())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(buildDir));
+// });
  
+
+// var transpile  = require('gulp-es6-module-transpiler');
+ 
+// gulp.task('build', function() {
+//     return gulp.src('src/**/*.js')
+//         .pipe(sourcemaps.init())
+//         .pipe(transpile({
+//             formatter: 'bundle'
+//         }))
+//         .pipe(sourcemaps.write('./'))
+//         .pipe(gulp.dest('lib'));
+//  })
+
+
+
+//  var babel = require('gulp-babel'),
+//     browserify = require('browserify'),
+//     source = require('vinyl-source-stream'),
+//     buffer = require('vinyl-buffer'),
+//     rename = require('gulp-rename'),
+//     uglify = require('gulp-uglify'),
+//     del = require('del');
+// var recast = require('recast');
+
+// gulp.task('clean-temp', function(){
+//   return del(['dest']);
+// });
+
+// gulp.task('es6-commonjs',['clean-temp'], function(){
+//   return gulp.src(['app/js/**/*.js'])
+//     .pipe(babel())
+//     .pipe(gulp.dest('dist/'));
+// });
